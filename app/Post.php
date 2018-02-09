29720b1e0fff9597700492a792b7acb2ccbf6d18
Alvaro Robles Andreu <alvaroroblessmr@gmail.com>
2018-02-09 18:49:20 +0100

    Post sin fecha de publicación y Post Programados

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -16,6 +17,13 @@ class Post extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class);
+    }
+
+    public function scopePublished($query)
+    {
+        $query->whereNotNull("published_at")
+            ->where("published_at",'<',Carbon::now())
+            ->latest('published_at');
     }
 
 }
